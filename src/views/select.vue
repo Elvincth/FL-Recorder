2f31532b674980eddb11b21deffc2a9093a08c29
Elvin Chu <elvincth@gmail.com>
2020-09-20 01:46:21 +0800

    Added size for thumnail

--- src/views/select.vue
+++ src/views/select.vue
@@ -1,7 +1,7 @@
 <!--Record windows select-->
 <template>
   <div>
-    <div v-for="(source,i) in sortArray(sources)" :key="i">
+    <div v-for="(source,i) in purifyArray(sources)" :key="i">
       <img :src="pngToB64(source.thumbnail.toPNG())" />
     </div>
   </div>
@@ -18,26 +18,38 @@ export default {
       sources: [],
       options: {
         types: ["window", "screen"],
+        thumbnailSize: {
+          width: 350,
+          height: 350,
+        },
         fetchWindowIcons: true,
       },
     };
   },
   methods: {
-    
-    sortArray(array) {
-      return array.slice().sort(function (a, b) {
-        return a.id.replace( /^\D+/g, '') > b.id.replace( /^\D+/g, '') ? 1 : -1;
+    //Array purify (Sort and delete unsed window)
+    purifyArray(array) {
+      //Filter dragged window
+      array = array.filter((item) => item.name !== "Drag");
+      //Sort by id
+      array = array.slice().sort(function (a, b) {
+        return a.id.replace(/^\D+/g, "") > b.id.replace(/^\D+/g, "") ? 1 : -1;
       });
+      return array;
     },
 
     async getSources() {
       let vm = this;
+
+      //Get sources update per 1 second
+
       await setInterval(async () => {
         await desktopCapturer.getSources(vm.options).then(async (sources) => {
           vm.sources = sources;
         });
       }, 1000);
     },
+
     pngToB64(pngImage) {
       //source.thumbnail.toPNG()
       return "data:image/png;base64," + new Buffer(pngImage).toString("base64");
