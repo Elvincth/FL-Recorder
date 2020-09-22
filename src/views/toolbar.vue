708ab1d2f3d85f4872bcea9cd2b078ef5084da70
Elvin Chu <elvincth@gmail.com>
2020-09-22 13:19:21 +0800

    Added items

--- src/views/toolbar.vue
+++ src/views/toolbar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="main">Toolbar view</div>
+  <div class="main">
+    <div class="item"></div>
+  </div>
 </template>
 
 <script>
@@ -17,12 +19,20 @@ body {
   height: 100%;
   background: transparent;
 }
+</style>
 
+<style lang="scss" scoped>
 .main {
   -webkit-app-region: drag;
   width: 500px;
   height: 70px;
   border-radius: 0.5rem;
   background-color: var(--background-color);
+  display: flex;
+  .item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
